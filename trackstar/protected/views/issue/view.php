fe8c3a61892e847c4ed1d252b99c53e4433d206c
vinothvetrivel <vinothvetrivel@gmail.com>
2014-10-09 19:01:26 +0530

    With model relationship

---
+++ trackstar/protected/views/issue/view.php
@@ -0,0 +1,32 @@
+<?php
+/* @var $this IssueController */
+/* @var $model Issue */
+
+$this->breadcrumbs=array(
+	'Issues'=>array('index'),
+	$model->name,
+);
+
+$this->menu=array(
+	array('label'=>'List Issue', 'url'=>array('index')),
+	array('label'=>'Create Issue', 'url'=>array('create')),
+	array('label'=>'Update Issue', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Issue', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Issue', 'url'=>array('admin')),
+);
+?>
+
+<h1>View Issue #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'name',
+		'description',
+		array('name'=>'type_id','value'=>CHtml::encode($model->getTypeText())),
+		array('name'=>'status_id','value'=>CHtml::encode($model->getStatusText())),
+		array('name'=>'owner_id','value'=>isset($model->owner) ? CHtml::encode($model->owner->username):'unknown'),
+		array('name'=>'requester_id','value'=>isset($model->requester)? CHtml::encode($model->requester->username):'unknown'),
+	),
+)); ?>
